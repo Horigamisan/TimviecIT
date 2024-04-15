9ab9bc2242c7c8795c1a49a5ae653005685109f7
Nguyễn Trọng Phúc <87481608+Horigamisan@users.noreply.github.com>
2024-04-15 09:07:14 +0700

    [Refactor]: add avatar of user from google OAuth

--- src/helpers/functions/saveFile.js
+++ src/helpers/functions/saveFile.js
@@ -1,19 +1,23 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-module.exports = (image, type = 'images') => {
-	const tmpPath = image.filepath;
-	const dir = path.join(__dirname, '../../../public/uploads/' + type);
-	const extension = image.originalFilename.split('.')[1];
-	const newFilename = image.newFilename + '-' + Date.now() + '.' + extension;
+module.exports = (image, type = "images") => {
+    const tmpPath = image.filepath;
+    const dir = path.join(__dirname, "../../../public/uploads/" + type);
+    const extension = image.originalFilename.split(".")[1];
+    const newFilename = image.newFilename + "-" + Date.now() + "." + extension;
 
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir);
-	}
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
 
-	fs.rename(tmpPath, path.join(dir, newFilename), (err) => {
-		if (err) throw err;
-	});
+    const newPath = path.join(dir, newFilename);
+    fs.copyFile(tmpPath, newPath, (err) => {
+        if (err) throw err;
+        fs.unlink(tmpPath, (err) => {
+            if (err) throw err;
+        });
+    });
 
-	return '/uploads/' + type + '/' + newFilename;
+    return "/uploads/" + type + "/" + newFilename;
 };
